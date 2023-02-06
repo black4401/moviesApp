ca627cd97a2b3b293342954bd88d0c4ffc4b7f80
Alexander Angelov <alexander.angelov@mentormate.com>
2023-02-06 15:34:22 +0200

    Initial Commit

---
+++ MoviesApp_Final/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesApp_Final
+//
+//  Created by Alexander Angelov on 6.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
